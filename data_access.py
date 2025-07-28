03a6e605c91adb74ef793ab093733add467ac826
kosonkh7 <khukwangha13@khu.ac.kr>
2025-07-28 15:40:40 +0900

    feat: Add 지표 설계.ipynb

--- data_access.py
+++ data_access.py
@@ -5,12 +5,12 @@ import pandas as pd
 conn = sqlite3.connect("db/sensor_data.sqlite")
 
 # SQL 쿼리 실행
-query = "SELECT * FROM sensor_record" # 테이블명 언젠가 바꿀 수도...
+query = "SELECT * FROM external_data" # 테이블명 언젠가 바꿀 수도...
 
 df = pd.read_sql_query(query, conn)
 print(df.info())
 
-print(df.head())
+print(df.head(30))
 
 # 연결 종료
 conn.close()
